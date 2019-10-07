2800fac00cbc811f8c39b81d0ffcba1249e1cf2d
11philip22 <philipwoldhek@gmail.com>
2019-10-07 12:41:15 +0200

    added new codebase

--- twitter_media_downloader_old.py
+++ twitter_media_downloader_old.py
@@ -1,8 +1,7 @@
-# WIP
-
-import os
+import logging
 import signal
 import sys
+from os import getcwd
 from pathlib import Path
 from pickle import dump
 from pickle import load
@@ -15,35 +14,14 @@ from urllib.parse import urlparse
 import requests
 import twint
 import youtube_dl
-from blessings import Terminal
 from bs4 import BeautifulSoup
-from progressbar import ProgressBar
 
-working_dir = os.getcwd()
-term = Terminal()
-
-
-class Writer(object):
-    def __init__(self, location):
-        self.location = location
-
-    def write(self, string):
-        with term.location(*self.location):
-            print(string)
-
-    def flush(self):
-        pass
-
-
-def write_to_screen(x_pos, y_pos, string):
-    location = (x_pos, y_pos)
-    w = Writer(location)
-    w.write(string)
+working_dir = getcwd()
 
 
 class Twitter(object):
-    def __init__(self, usernames, location, get_videos=True, get_photos=True,
-                 ignore_errors=True, sleep_timer=1, output=True):
+    def __init__(self, usernames, location, get_videos=True, keep_log=True,
+                 ignore_errors=True, get_photos=True, logger=""):
         self.get_photos = get_photos
         self.get_videos = get_videos
         self.queue = Queue()
@@ -51,9 +29,9 @@ class Twitter(object):
         self.usernames = usernames
         self.ignore_errors = ignore_errors
         self.download_folder = Path(location, "twitter")
-        self.writer = Writer((0, 4))
-        self.output = output
-        self.sleeptimer = sleep_timer
+        self.logging = keep_log
+        if self.logging:
+            self.logger = logging.getLogger(logger)
 
     @staticmethod
     def get_soup(html):
@@ -63,16 +41,14 @@ class Twitter(object):
         else:
             return
 
-    def get_tweets(self, target):
+    @staticmethod
+    def get_tweets(target):
         c = twint.Config()
         c.Username = target
         c.Resume = str(Path(working_dir, "resume", "{0}_history_ids.txt".format(target)))
         c.Store_object = True
         c.Hide_output = True
         c.Media = True
-        if self.output is True:
-            write_to_screen(0, 0, "Twitter crawler:")
-            write_to_screen(0, 1, "crawling {0}".format(target))
         twint.run.Search(c)
         tweets = twint.output.tweets_list
 
@@ -97,34 +73,33 @@ class Twitter(object):
                 location.mkdir()
             if not photo_location.is_dir():
                 photo_location.mkdir()
-            prefix_msg = "{0}: Downloading photos ".format(target)
-            with ProgressBar(max_value=len(urls), fd=self.writer, prefix=prefix_msg) as pbar:
-                current_len = 0
-                headers = {
-                    'User-Agent':
-                        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) \
-                        Chrome/74.0.3729.169 Safari/537.36'}
-                for tweet in urls:
+            headers = {
+                'User-Agent':
+                    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) \
+                    Chrome/74.0.3729.169 Safari/537.36'}
+            for tweet in urls:
+                try:
                     result = requests.get(tweet, headers)
-                    if result.status_code is 200:
-                        content = result.content
-                        soup = self.get_soup(content)
-                        for link in soup.findAll('img', attrs={'src': compile("^https://pbs.twimg.com/media")}):
-                            photo_url = link['src']
-                            url_obj = urlparse(photo_url)
-                            file_name = url_obj.path.replace("/media/", "")
-                            path = str(Path(photo_location, file_name))
-                            if not os.path.isfile(path):
-                                with open(path, "wb") as file:
-                                    file.write(requests.get(photo_url).content)
-                    else:
-                        print("Error requesting the webpage: {0}".format(result.status_code))
-                        if self.ignore_errors is False:
-                            exit(1)
-                    if self.output is True:
-                        current_len += 1
-                        pbar.update(current_len)
-                        write_to_screen(0, 3, "Twitter downloader:")
+                except Exception as e:
+                    self.logger.error(e)
+                    if not self.ignore_errors:
+                        exit(1)
+                    continue
+                if result.status_code is 200:
+                    content = result.content
+                    soup = self.get_soup(content)
+                    for link in soup.findAll('img', attrs={'src': compile("^https://pbs.twimg.com/media")}):
+                        photo_url = link['src']
+                        url_obj = urlparse(photo_url)
+                        file_name = url_obj.path.replace("/media/", "")
+                        path = str(Path(photo_location, file_name))
+                        if not Path(path).is_file():
+                            with open(path, "wb") as file:
+                                file.write(requests.get(photo_url).content)
+                else:
+                    self.logger.error("Error requesting the webpage: {0}".format(result.status_code))
+                    if not self.ignore_errors:
+                        exit(1)
 
     def download_videos(self, target, urls):
         if urls:
@@ -134,27 +109,28 @@ class Twitter(object):
                 location.mkdir()
             if not video_location.is_dir():
                 video_location.mkdir()
-            prefix_msg = "{0}: Downloading videos ".format(target)
-            with ProgressBar(max_value=len(urls), fd=self.writer, prefix=prefix_msg) as pbar:
-                current_len = 0
-                for tweet in urls:
-                    try:
-                        download_path = str(Path(video_location, "%(id)s.%(ext)s"))
-                        ydl_opts = {"outtmpl": download_path, "quiet": True}
-                        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                            ydl.download([tweet, ])
-                    except youtube_dl.utils.DownloadError as y:
-                        print(y)
-                        if self.ignore_errors is False:
-                            exit(1)
-                    if self.output is True:
-                        current_len += 1
-                        pbar.update(current_len)
-                        write_to_screen(0, 3, "Twitter downloader:")
-                    if len(urls) > 200:
-                        sleep(self.sleeptimer)
+            for tweet in urls:
+                try:
+                    download_path = str(Path(video_location, "%(id)s.%(ext)s"))
+                    ydl_opts = {
+                        "outtmpl": download_path,
+                        "quiet": True,
+                        "logger": self.logger,
+                        "ignoreerrors:": self.ignore_errors
+                    }
+                    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                        ydl.download([tweet, ])
+                except Exception as y:
+                    self.logger.error(y)
+                    if not self.ignore_errors:
+                        exit(1)
+                if len(urls) > 200:
+                    sleep(2)
 
-    def sigterm_handler(self, signal, frame):
+    def sigterm_handler(self):
+        """
+        possible parameters: signal, frame
+        """
         self.dump_queue()
         sys.exit(0)
 
@@ -171,25 +147,60 @@ class Twitter(object):
             self.download_folder.mkdir()
         while not self.queue.empty() or self.crawling:
             tweets = self.queue.get()
-            if self.get_photos is True:
+            if self.logging:
+                if not tweets[1] and not tweets[2]:
+                    pass
+                elif not tweets[1] and tweets[2]:
+                    self.logger.info("{0}: downloading {1} videos".format(tweets[0], len(tweets[2])))
+                elif not tweets[2] and tweets[1]:
+                    self.logger.info("{0}: downloading {1} photos".format(tweets[0], len(tweets[1])))
+                else:
+                    self.logger.info(
+                        "{0}: downloading {1} photos and {2} videos".format(tweets[0], len(tweets[1]), len(tweets[2])))
+            if self.get_photos:
                 self.download_photos(tweets[0], tweets[1])
-            if self.get_videos is True:
+                if self.logging:
+                    if not tweets[1]:
+                        pass
+                    else:
+                        self.logger.info("{0}: photo download complete!".format(tweets[0]))
+            if self.get_videos:
                 self.download_videos(tweets[0], tweets[2])
-        if self.output is True:
-            write_to_screen(0, 4, "Done downloading!")
+                if self.logging:
+                    if not tweets[2]:
+                        pass
+                    else:
+                        self.logger.info("{0}: video download complete!".format(tweets[0]))
+            if self.queue.qsize() > 0:
+                self.logger.info("{0} items left in the download queue".format(self.queue.qsize()))
+        if self.logging:
+            self.logger.info("Done downloading!")
 
     def crawler(self):
-        if not os.path.exists("resume"):
-            os.mkdir("resume")
+        resume_location = Path(working_dir, "resume")
+        if not resume_location.is_dir():
+            resume_location.mkdir()
+        counter = len(self.usernames)
         for username in self.usernames:
+            if self.logging:
+                self.logger.info("crawling {0}".format(username))
             tweets = self.get_tweets(username)
             self.queue.put(tweets)
+            counter -= 1
+            if self.logging:
+                self.logger.info("{0} items left in crawler queue".format(counter))
         self.crawling = False
-        if self.output is True:
-            write_to_screen(0, 1, "Done crawling!")
+        if self.logging:
+            self.logger.info("Done crawling!")
 
     def start(self):
-        os.system("clear")
+        if self.logging:
+            self.logger.info("Starting twitter module with a queue size of {0}".format(len(self.usernames)))
         signal.signal(signal.SIGTERM, self.sigterm_handler)
         Thread(target=self.downloader).start()
         self.crawler()
+
+
+def worker(usernames, location):
+    t = Twitter(usernames=usernames, location=location, logger="SMDL.Twitter")
+    t.start()
